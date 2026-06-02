28c57f4bdcbc0deed0c3e34a3d528a36703438cb
AHMAD HASSAN <aahmadhassan2007@gmail.com>
2026-06-02 07:07:54 +0500

    Update repo-search.js

--- plugins/repo-search.js
+++ plugins/repo-search.js
@@ -25,7 +25,7 @@ cmd({
     responseMsg += `🍴 *Forks*: ${data.forks_count}\n`;
     responseMsg += `👤 *Owner*: ${data.owner.login}\n`;
     responseMsg += `📅 *Created At*: ${new Date(data.created_at).toLocaleDateString()}\n`;
-    responseMsg += `\n> *© Powered by AHMADTechX*`;
+    responseMsg += `\n> *© Powered by 𝐀͢ͱ꧊ϻ͒͜𝛂͜𝛛🚩*`;
 
     await conn.sendMessage(from, { text: responseMsg }, { quoted: m });
   } catch (error) {

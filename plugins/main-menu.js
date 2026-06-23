5dbaf4260a9b65bbe240d76250e3dbd1b8985a32
AHMAD HASSAN <aahmadhassan2007@gmail.com>
2026-06-23 12:14:00 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -74,7 +74,7 @@ ${menuSections}
                 forwardingScore: 999, 
                 isForwarded: true, 
                 forwardedNewsletterMessageInfo: { 
-                    newsletterJid: '120363408512260657@newsletter', 
+                    newsletterJid: '120363426472060176@newsletter', 
                     newsletterName: "AHMADTech", 
                     serverMessageId: 143 
                 } 
